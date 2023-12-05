f179f1691484551b61db6de846407ac0e4daf577
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 14:01:07 +0200

    Add project files.

---
+++ TheReturnKey/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheReturnKey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(cube(5));
+
+            cube(5);
+
+            Console.ReadLine();
+
+            int cube(int num)
+            {
+                int results = num * num * num;
+                return results;
+            }
+        }
+    }
+}
